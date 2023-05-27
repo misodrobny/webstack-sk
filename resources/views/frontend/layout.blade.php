a51fdfacf1e84e636571e62938a0e096101adc14
Michal Drobny <michal.drobny2me.com>
2023-05-27 12:07:07 +0200

    Favicon update

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -7,10 +7,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Webstack | Landing Page</title>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("apple-touch-icon.png") }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("favicon-32x32.png") }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon-16x16.png") }}">
+        <link rel="manifest" href="{{ asset("site.webmanifest") }}">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
 
@@ -39,6 +39,7 @@
         @yield('content')
 
         @include('frontend.component.ProposeTalkModal')
+        @include('frontend.component.SpeakerModal')
 
         <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
