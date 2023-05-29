30463de2dce310d91bc9f2212f50291c817108ad
Michal Drobny <michal.drobny2me.com>
2023-05-29 12:43:35 +0200

    Book your tickets color change

--- resources/views/frontend/component/CarouselBookYourTickets.blade.php
+++ resources/views/frontend/component/CarouselBookYourTickets.blade.php
@@ -1,16 +1,16 @@
-<div id="autoSlideContainer" class="md:min-h-[220px] py-2 lg:py-0">
+<div id="autoSlideContainer" class="md:min-h-[220px] py-2 lg:py-0 bg-turquoise-500">
     <a href="https://www.inviton.eu/e-14216/webstack"
-        class="text-[44px] md:text-[190px] tracking-[-0.02em] leading-[47px] md:leading-[204px] coolvetica-bold ml-[18px] md:ml-20 text-turquoise-500 min-w-max slider"
+        class="text-[44px] md:text-[190px] tracking-[-0.02em] leading-[47px] md:leading-[204px] coolvetica-bold ml-[18px] md:ml-20 text-dark-teal min-w-max slider"
         title="Book your ticket on invition.eu now!" target="_blank">
         _Book your tickets
     </a>
     <a href="https://www.inviton.eu/e-14216/webstack"
-        class="text-[44px] md:text-[190px] tracking-[-0.02em] leading-[47px] md:leading-[204px] coolvetica-bold ml-[18px] md:ml-20 text-turquoise-500 min-w-max slider"
+        class="text-[44px] md:text-[190px] tracking-[-0.02em] leading-[47px] md:leading-[204px] coolvetica-bold ml-[18px] md:ml-20 text-dark-teal min-w-max slider"
         title="Book your ticket on invition.eu now!" target="_blank">
         _Book your tickets
     </a>
     <a href="https://www.inviton.eu/e-14216/webstack"
-        class="text-[44px] md:text-[190px] tracking-[-0.02em] leading-[47px] md:leading-[204px] coolvetica-bold ml-[18px] md:ml-20 text-turquoise-500 min-w-max slider"
+        class="text-[44px] md:text-[190px] tracking-[-0.02em] leading-[47px] md:leading-[204px] coolvetica-bold ml-[18px] md:ml-20 text-dark-teal min-w-max slider"
         title="Book your ticket on invition.eu now!" target="_blank">
         _Book your tickets
     </a>
